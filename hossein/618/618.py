cc0848d5f992a7675b1305b50f2d2db8fee4018c
Hossein Dehghan <mh.dehghan@ut.ac.ir>
2020-04-23 19:56:52 +0430

    Cleaning

--- hossein/618/618.py
+++ hossein/618/618.py
@@ -4,13 +4,13 @@
 
 diameter = int(input())
 diameter += 1
-STAR="*"
-SPACE=" "
+STAR = "*"
+SPACE = " "
 for i in range(diameter):
-    print(SPACE*(diameter-i-1),end='')
-    print(STAR * (i*2 + 1),end='')
+    print(SPACE * (diameter - i - 1), end='')
+    print(STAR * (i * 2 + 1), end='')
     print()
 for i in reversed(range(diameter - 1)):
-    print(SPACE*(diameter-i-1),end='')
-    print(STAR*(i*2+1),end='')
+    print(SPACE * (diameter - i - 1), end='')
+    print(STAR * (i * 2 + 1), end='')
     print()
