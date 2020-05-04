1d2261fb059d4821e2b26c4ede777fa37e729583
Hossein Dehghan <mh.dehghan@ut.ac.ir>
2020-05-05 00:55:48 +0430

    Add a solution for 10231.

---
+++ hossein/contest/10231/10231.py
@@ -0,0 +1,11 @@
+# https://quera.ir/problemset/contest/10231/
+
+MOLANA_HAFEZ = ['MOLANA', 'HAFEZ']
+input_text = [input() for i in range(5)]
+
+x = 'N-MOLANA1'
+result = []
+for i, line in enumerate(input_text):
+    if MOLANA_HAFEZ[0] in line or MOLANA_HAFEZ[1] in line:
+        result.append(i + 1)
+print(*result) if len(result) > 0 else print('NOT FOUND!')
